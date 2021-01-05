2a0fd4e5fa724a7581807a713f744f6a4d03edf4
Cesar Fidel <cesar__fidel@hotmail.com>
2021-01-05 02:08:13 -0600

    Obtiene publicaciones de firestore

--- lib/Feed.dart
+++ lib/Feed.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Feed extends StatefulWidget{
     @override
     States createState()=> States();
     //throw UnimplementedError();
-
 }
-
+final databaseReference2 = Firestore.instance;
 class States extends State<Feed>
 {
   final searchcontroller = new TextEditingController();
+  final databaseReference = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +29,90 @@ class States extends State<Feed>
             ]
         ),
       body:
-        Container(
-          child: ListView(
-            children: [],
-          )
-        ),
+          Column(
+            children: <Widget>[
+              Container(
+                child: FutureBuilder(
+                  future: getData(),
+                  builder: (context, snapshot)
+                  {
+                    if(snapshot.connectionState == ConnectionState.waiting)
+                      return Column(
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            child: Column(children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 6, 8, 3),
+                                child:  Text("Cargando",textAlign: TextAlign.center, style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                              )
+                            ]
+                            )
+
+                        ),
+                      ]);
+                    else
+                      {
+                        return ListView(
+                          shrinkWrap: true,
+                          children: [
+                              SizedBox(
+                              height: MediaQuery. of(context). size. height - 129,
+                              child: ListView.builder(
+                                  shrinkWrap: true,
+                                  itemCount: snapshot.data.length,
+                                  itemBuilder: (context, index)
+                                  {
+                                    return
+                                      Column(
+                                      children: <Widget>[
+                                        Container(
+                                          alignment: Alignment.center,
+                                          child: Column(children: <Widget>[
+                                            Row(
+                                              children: <Widget>[
+                                              Padding(
+                                                padding: const EdgeInsets.fromLTRB(8, 6, 8, 3),
+                                                child:  Text(snapshot.data[index].data()["usuario"],textAlign: TextAlign.left, style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                                              )
+                                              ],
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.fromLTRB(8, 6, 8, 6),
+                                              child:  Text(snapshot.data[index].data()["descripcion"],textAlign: TextAlign.center),
+                                            )
+
+                                            ]
+                                          ),
+                                          margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+                                          decoration: new BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius:BorderRadius.circular(7),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: Colors.grey.withOpacity(0.5),
+                                                spreadRadius: 5,
+                                                blurRadius: 7,
+                                                offset: Offset(0, 3), // changes position of shadow
+                                              ),
+                                            ],
+                                          ),
+
+                                        )
+                                      ],
+                                    );
+                                  }
+                              ),
+                            ),
+                          ],
+                        );
+                      }
+                  },
+                ),
+              ),
+            ]
+          ),
+
       drawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
@@ -79,4 +159,10 @@ class States extends State<Feed>
       ),
     );
   }
+}
+
+Future getData() async
+{
+  QuerySnapshot q = await databaseReference2.collection('publicacion').get();
+  return q.docs;
 }
