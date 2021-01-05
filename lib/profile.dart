7d40d5790ec0dc6ee35369481d1f1b06789b9104
Erick Canto <kucho_super.68@hotmail.com>
2021-01-04 22:38:39 -0600

    Se actualizo el perfil

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'popup.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class profile extends StatefulWidget{
@@ -8,13 +9,16 @@ class profile extends StatefulWidget{
 
 //Lo dificil va a ser hacer esto dinámico, en teoría
 //deberíamos hacer que a este metodo reciba algun parametro para identificar el
-//perfil y con ese ID hacer la petición de flutter
+//perfil y con ese ID hacer la petición de firebase
 //Espero que sepas como adaptar el código para que se pueda mostrar la información
 //correspondiente al perfil dependiendo de lo obtenido por firebase
 //Todo esto creo que se puede hacer con stateless widgets pero no estoy seguro
 
+enum WhyFarther { harder, smarter, selfStarter, tradingCharter }
 class stateprofile extends State<profile>
 {
+  var _selection = WhyFarther.tradingCharter;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +62,7 @@ class stateprofile extends State<profile>
                     ],
                   ),
                   Padding(
-                      padding: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(16),
                       child: Container(
                         padding: EdgeInsets.all(10.0),
                         decoration: BoxDecoration(
@@ -68,7 +72,39 @@ class stateprofile extends State<profile>
                             "sté usando la aplicacion, solo espero que no se des"
                             "borde el texto porque me mato",textAlign: TextAlign.center, style: TextStyle(color: Colors.black87,fontSize: 15),),
                       ),
+                  ),
+                  //TODO: Este sería el boton para reportar y cosas así. Aun no se como va a ir
+                  //TODO: Yo creo que quizá no es necesario que funcione
+                  PopupMenuButton<WhyFarther>(
+                    onSelected: (WhyFarther result) { setState(() { _selection = result; }); },
+                    itemBuilder: (BuildContext context) => <PopupMenuEntry<WhyFarther>>[
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.harder,
+                        child: Text('Working a lot harder'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.smarter,
+                        child: Text('Being a lot smarter'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Being a self-starter'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.tradingCharter,
+                        child: Text('Placed in charge of trading charter'),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width)*0.8,
+                    child: Divider(
+                      color: Colors.black12,
+                      thickness: 3,
+                    ),
                   )
+                  //TODO: A partir de aqui se comenzaría a llenar con cosas de la persona
+                  //TODO: Voy a investigar la forma de hacerlo fácil
                 ],
               )
             ],
